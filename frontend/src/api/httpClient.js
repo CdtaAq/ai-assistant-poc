511c1d84f6332b4a133c14851111b0e0b024f44f
chris Taylor <ctay456a@gmail.com>
2025-09-22 17:32:15 -0400

    Create httpClient.js

---
+++ frontend/src/api/httpClient.js
@@ -0,0 +1,8 @@
+export async function postJson(url, body) {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(body)
+  });
+  return res.json();
+}
